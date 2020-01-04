39912dda2fcccbf7bd90478b2cfaba8f3f889e8e
Philip Darlington <pad.bfc@gmail.com>
2020-01-04 20:14:54 +0000

    wildcard handling

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,18 @@ Route::get('test', function(){
 Route::get('/', function(){
     return view('welcome');
 });
+
+Route::get('/posts/{post}', function($post){
+    $posts = [
+        'my-first-post' => 'Hello, this is my first post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+
+    if (!array_key_exists($post, $posts)){
+        abort(404, 'Sorry that post was not found.');
+    }
+
+    return view('post',[
+        'post' => $posts[$post]
+    ]);
+});

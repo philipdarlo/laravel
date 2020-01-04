4d38955e36bd6da779bef7ee1069ce8af2e8fae2
Philip Darlington <pad.bfc@gmail.com>
2020-01-04 20:57:46 +0000

    first look at eloquent ep10

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+use App\Posts;
+
 class PostsController extends Controller
 {
     public function show($slug)
-    {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-    
+    {    
         return view('post',[
-            'post' => $post
+            'post' => Posts::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
